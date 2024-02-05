0c2f9bedc2d18b4c75bf70ed86197f3b31119cf8
dahiiv2 <danilnmellera@gmail.coM>
2024-02-05 19:07:43 +0100

    Conflicto resuelto

--- mainn.dart
+++ mainn.dart
@@ -4,10 +4,6 @@ void main(List<String> arguments) {
     // Formatear la fecha y hora
     String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
     // Imprimir el mensaje con la fecha y hora
-<<<<<<< HEAD
     print("Conflicto!!!!!");
-=======
-    print("Conflicto!!!!!!!!!!!!!!");
->>>>>>> rama-nueva
     print('Hello world: ($formattedDateTime)');
 }
