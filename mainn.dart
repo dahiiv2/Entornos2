88ce62f3b251384f97d576a7918492a88893a37b
dahiiv2 <danilnmellera@gmail.coM>
2024-02-05 18:07:43 +0100

    Dart en la rama Spanish

--- mainn.dart
+++ mainn.dart
@@ -1,3 +1,8 @@
-void main() {
-  print("Hola Mundo!");
+void main(List<String> arguments) {
+    // Obtener la fecha y hora actual
+    DateTime now = DateTime.now();
+    // Formatear la fecha y hora
+    String formattedDateTime = "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+    // Imprimir el mensaje con la fecha y hora
+    print('Hello world: ($formattedDateTime)');
 }
